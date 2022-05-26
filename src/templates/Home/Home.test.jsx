931ddb9c3eb609b37d4d3fd1b8bf93061ad6e1cf
Geovanne <callegarog00@gmail.com>
2022-05-26 09:44:19 -0300

    testing the heading using jest and with 100 coverage

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -1,16 +1,8 @@
 import { renderTheme } from '../../styles/render-theme';
 import Home from '.';
-import { screen } from '@testing-library/react';
-import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Oi',
-  }).parentElement;
-
-  expect(headingContainer).toHaveStyle({
-    background: theme.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  debug();
 });
