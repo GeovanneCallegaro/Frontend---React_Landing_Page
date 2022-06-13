774ea57cb5b9d70947ac8bbd76018c32010de355
Geovanne <callegarog00@gmail.com>
2022-06-13 16:52:49 -0300

    GridContent component and making changes to the TextComponent component to receive html from the api

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,25 @@
+import * as C from './styles';
+import P from 'prop-types';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <C.Container>
+        <Heading uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <C.Html></C.Html>
+        <TextComponent>{html}</TextComponent>
+      </C.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  background: P.bool,
+};
